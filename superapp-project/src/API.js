a518ac93217668c93a2a6a63143e23b4de267799
Devbrotho Nath <devbrotho.dn@gmail.com>
2023-10-03 21:32:12 +0530

    category page function

---
+++ superapp-project/src/API.js
@@ -0,0 +1,24 @@
+const MoviesArr = [
+  {
+    id: 1,
+    title: "Action",
+  },
+  {
+    id: 2,
+    title: "Horror",
+  },
+  {
+    id: 3,
+    title: "Romance",
+  },
+  {
+    id: 4,
+    title: "Fantasy",
+  },
+  {
+    id: 5,
+    title: "Fiction",
+  },
+];
+
+export default MoviesArr;
--- superapp-project/src/pages/categoryPage/categoryPage.jsx
+++ superapp-project/src/pages/categoryPage/categoryPage.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import MoviesArr from "../../API";
+import { useAppContext } from "../../components/context/appContext";
 
 const CategoryPage = () => {
-  return <div>categoryPage</div>;
+  console.log(MoviesArr);
+  //   const [MoviesArr, setMovieArr] = useState([]);
+  const { favorites, addToFavorites, removeFromFavorites } = useAppContext();
+
+  console.log(favorites);
+
+  const favoritesChecker = (id) => {
+    const boolean = favorites.some((movie) => movie.id === id);
+    return boolean;
+  };
+
+  return (
+    <div>
+      {MoviesArr.map((movie) => (
+        <div key={movie.id}>
+          <div>
+            <h2>{movie.title}</h2>
+            {favoritesChecker(movie.id) ? (
+              <button onClick={() => removeFromFavorites(movie.id)}>
+                Remove
+              </button>
+            ) : (
+              <button onClick={() => addToFavorites(movie)}>Add</button>
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default CategoryPage;
